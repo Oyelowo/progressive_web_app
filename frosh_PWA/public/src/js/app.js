6fef9d71ba36f4def5ed1400f3d4dfd6d3573f5f
Oyelowo <oyelowooyedayo@gmail.com>
2018-10-19 21:24:18 +0300

    getting started with beforeinstallprompt

--- frosh_PWA/public/src/js/app.js
+++ frosh_PWA/public/src/js/app.js
@@ -1,6 +1,13 @@
+let deferredPrompt;
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("../../sw.js").then(function() {
     console.log("service worker registered!");
   });
 }
 
+window.addEventListener("beforeinstallprompt", function(event) {
+  console.log("beforeinstallprompt fired");
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
